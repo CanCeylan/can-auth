8982493bec9097d293dca6a1cd92b9fe28ae165a
Orhan Can Ceylan <orhancanceylan@gmail.com>
2012-11-19 18:07:59 +0100

    Ciao

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CanAuth::Application.config.secret_token = '1d56cb731d20ad17481a65109c2d269c0c787b68e137c469e9c2df197b372a669b392ab1c05d8c1230c85b9b80a7de1e57884f55a98ca1ac50f1f762295150fa'
